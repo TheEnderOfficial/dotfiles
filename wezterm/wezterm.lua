8d7499923520c4ea6966fd232e846216a313867b
Ender Official <enderofficial.ml@gmail.com>
2023-03-13 13:18:07 +0300

    Upload wezterm cfg

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm';
+
+return {
+  font = wezterm.font("JetBrains Mono", {weight="Bold", italic=false}),
+  color_scheme = "Pretty and Pastel (terminal.sexy)",
+  enable_scroll_bar = true,
+}
